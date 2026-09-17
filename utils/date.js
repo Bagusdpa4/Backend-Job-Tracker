371f2fe4e63a9e0383fa2d7bed5526118b711ac3
Bagus Dwi <sugab.dwi88@gmail.com>
2026-09-17 18:17:55 +0700

    format date

--- utils/date.js
+++ utils/date.js
@@ -8,10 +8,20 @@ function parseDateAsWIB(dateString) {
 }
 
 // Format Date (tersimpan sbg UTC) balik ke "YYYY-MM-DD" versi WIB (UTC+7)
+const HARI = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+const BULAN = [
+  "Januari", "Februari", "Maret", "April", "Mei", "Juni",
+  "Juli", "Agustus", "September", "Oktober", "November", "Desember",
+];
+
 function formatDateToWIB(date) {
   if (!date) return null;
   const wib = new Date(date.getTime() + 7 * 60 * 60 * 1000);
-  return wib.toISOString().slice(0, 10);
+  const hari = HARI[wib.getUTCDay()];
+  const tanggal = wib.getUTCDate();
+  const bulan = BULAN[wib.getUTCMonth()];
+  const tahun = wib.getUTCFullYear();
+  return `${hari}, ${tanggal} ${bulan} ${tahun}`;
 }
 
 function withWIBDate(application) {
