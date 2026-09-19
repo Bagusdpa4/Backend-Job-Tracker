9f66113c7f8cd291fc183e095e1814a8c179ad81
Bagus Dwi <sugab.dwi88@gmail.com>
2026-09-19 20:44:51 +0700

    Swagger

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const authRouter = require("./auth.routes");
 const applicationsRouter = require("./applications.routes"); 
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yaml");
+const fs = require("fs");
+const path = require("path");
 
 const router = express.Router();
+
+const swagger_path = path.resolve(__dirname, "../docs/api-docs.yaml");
+const customCssUrl =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css";
+const customJs = [
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js",
+];
+const file = fs.readFileSync(swagger_path, "utf-8");
+
+// // API Docs
+const swaggerDocument = YAML.parse(file);
+router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument, { customCssUrl, customJs }));
 
 router.use("/api/v1", authRouter);
 router.use("/api/v1", applicationsRouter);
